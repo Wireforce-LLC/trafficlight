d3e46d51e1f40c2388c60a6fd3b119f27a8fc4aa
Maslow Vladislav <70482199+masloffvs@users.noreply.github.com>
2024-03-17 18:00:20 +0300

    now all routers requests compact in /system/routers

--- 0http/systemRouters.js
+++ 0http/systemRouters.js
@@ -1,17 +1,43 @@
 const {Http} = require("../src/Http");
 const {Router} = require("../src/Router");
+const _ = require("lodash");
 
 module.exports = (router) => {
   router.get(
     "/system/routers",
     Http.basicAuthMiddleware("admins"),
     async (req, res, next) => {
+      const type = _.get(req, 'query.type', 'plain')
 
-      Http
-        .of(req, res)
-        .sendJsonObject(
-          Http.positive(Router.getAllExistsRoutes())
-        )
+      switch (type) {
+        case "nested":
+          return Http
+            .of(req, res)
+            .sendJsonObject(
+              Http.positive(Router.getAllRoutersAsNestedFolders())
+            )
+
+        case "folders":
+          return Http
+            .of(req, res)
+            .sendJsonObject(
+              Http.positive(Router.getAllFolders())
+            )
+
+        case "contents":
+          return Http
+            .of(req, res)
+            .sendJsonObject(
+              Http.positive(Router.getAllExistsRoutesContents())
+            )
+
+        default:
+          return Http
+            .of(req, res)
+            .sendJsonObject(
+              Http.positive(Router.getAllExistsRoutes())
+            )
+      }
     }
   )
 }
--- 0http/systemRoutersFolders.js
+++ 0http/systemRoutersFolders.js
@@ -1,6 +1,10 @@
 const {Http} = require("../src/Http");
 const {Router} = require("../src/Router");
 
+/**
+ * @deprecated
+ * @param router
+ */
 module.exports = (router) => {
   router.get(
     "/system/routers/folders",
