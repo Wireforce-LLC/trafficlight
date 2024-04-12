710ffc519acde7299972eee09c8c97a3e0fa1e9b
Maslow Vladislav <70482199+masloffvs@users.noreply.github.com>
2024-04-12 03:47:25 +0300

    add JSDocs

--- kits/DatabaseKit.js
+++ kits/DatabaseKit.js
@@ -1,13 +1,20 @@
-const moment = require("moment/moment");
+const moment = require("moment");
 const { MongoClient } = require("mongodb");
 const { logger } = require("../src/Logger");
 const { $configuratorKit } = require("./ConfiguratorKit");
 const { $loggerKit } = require("./LoggerKit");
 
+/**
+ * Represents a database client specifically for MongoDB.
+ */
 class DatabaseClient {
   static ip = $configuratorKit.get("databases.mongodb.ip");
   static port = $configuratorKit.get("databases.mongodb.port");
 
+  /**
+   * Constructs the DatabaseClient instance, ensuring MongoDB IP and port are specified.
+   * @throws {Error} If the IP or port for MongoDB is not specified.
+   */
   constructor() {
     if (!DatabaseClient.ip) {
       throw new Error("MongoDB IP not specified. Please provide a valid IP in the configurator.");
@@ -26,12 +33,25 @@ class DatabaseClient {
     this.client = new MongoClient(url);
   }
 
+  /**
+   * Gets the ready-to-use MongoClient instance.
+   * @returns {MongoClient} The MongoClient instance.
+   */
   getReadyClient() {
     return this.client;
   }
 }
 
+/**
+ * Encapsulates database client handling.
+ */
 class DatabaseKit {
+  /**
+   * Creates an instance of DatabaseKit.
+   * @param {Object} param0 - The parameter object.
+   * @param {DatabaseClient} param0.databaseClient - The database client.
+   * @throws {Error} If the 'databaseClient' parameter is not passed.
+   */
   constructor({ databaseClient }) {
     if (!databaseClient) {
       throw Error("The 'databaseClient' parameter is not passed");
@@ -41,11 +61,18 @@ class DatabaseKit {
   }
 }
 
+/**
+ * Acts as a proxy to the database, specifically for handling traffic data.
+ */
 class DatabaseProxy extends DatabaseKit {
   static _pathDb_traffic = $configuratorKit.getDbPath("db/traffic");
   static _pathDb_trafficRouters =
     $configuratorKit.getDbPath("db/traffic/routers");
 
+  /**
+   * Constructs the DatabaseProxy instance.
+   * @param {Object} props - The properties passed to the DatabaseKit constructor.
+   */
   constructor(props) {
     super(props);
 
@@ -60,6 +87,11 @@ class DatabaseProxy extends DatabaseKit {
     });
   }
 
+  /**
+   * Pushes an HTTP request into the analytic database.
+   * @param {Object} httpRequest - The HTTP request to push.
+   * @returns {Promise} The result of the insert operation.
+   */
   async pushHttpRequest(httpRequest) {
     $loggerKit.getLogger().debug("Pushed http request in analytic database");
 
@@ -70,6 +102,14 @@ class DatabaseProxy extends DatabaseKit {
       .insertOne(httpRequest);
   }
 
+  /**
+   * Counts HTTP requests based on the provided query and time range.
+   * @param {Object} query - The query to match documents.
+   * @param {Object} param1 - The parameters for time range and filtering.
+   * @param {String} param1.startTime - The start time for filtering.
+   * @param {String} param1.endTime - The end time for filtering.
+   * @returns {Promise} The count of documents matching the criteria.
+   */
   async countHttpRequest(
     query,
     {
@@ -90,6 +130,16 @@ class DatabaseProxy extends DatabaseKit {
       });
   }
 
+  /**
+   * Finds HTTP requests based on the provided query and time range.
+   * @param {Object} query - The query to match documents.
+   * @param {Object} param1 - The parameters for time range, sorting, and limiting.
+   * @param {String} param1.startTime - The start time for filtering.
+   * @param {String} param1.endTime - The end time for filtering.
+   * @param {Number} param1.sort - The sort order (1 for ascending, -1 for descending).
+   * @param {Number} param1.limit - The maximum number of documents to return.
+   * @returns {Promise} The documents matching the criteria.
+   */
   async findHttpRequests(
     query,
     {
@@ -115,6 +165,14 @@ class DatabaseProxy extends DatabaseKit {
       .toArray();
   }
 
+  /**
+   * Finds and removes HTTP requests based on the provided query and time range.
+   * @param {Object} query - The query to match documents for deletion.
+   * @param {Object} param1 - The parameters for time range.
+   * @param {String} param1.startTime - The start time for filtering.
+   * @param {String} param1.endTime - The end time for filtering.
+   * @returns {Promise} The result of the delete operation.
+   */
   async findAndRemoveHttpRequests(
     query,
     {
@@ -137,7 +195,6 @@ class DatabaseProxy extends DatabaseKit {
 }
 
 const databaseClient = new DatabaseClient();
-const $databaseKit = new DatabaseKit({ databaseClient });
 const $databaseKitProxy = new DatabaseProxy({ databaseClient });
 
 module.exports = { $databaseKit: $databaseKitProxy };
